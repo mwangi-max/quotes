ef1b0d4480589cf1232f8d90619a78b07484d2a3
Kelvin Kamau <kelvinkam24@gmail.com>
2022-11-30 13:45:27 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import quotes from './quotes';
+import './index.css';
 
-function App() {
+const App = () => {
+
+ const [count,setCount] = useState(0);
+ const [text,setText] = useState([]);
+
+ const handleClick = (e) =>{
+  e.preventDefault();
+
+  let amount = parseInt(count);
+
+  if(count <= 0){
+    amount = 1;
+  }
+  if(count > quotes.length-1){
+    amount = quotes.length;
+  }
+
+  setText(quotes.slice(0,amount));
+ }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      
+
+      <section>
+     <form onSubmit={handleClick}>
+      
+
+      <div className='input-container'>
+           
+            <input type='number' value={count} onChange={(e)=>setCount(e.target.value)}/>
+
+            <button type='submit'>QUOTE(S)</button>
+
+      </div>
+     
+
+     </form>
+     {
+      text.map((qoute,index)=>{
+       return <article key={index}>
+        <p><strong>{qoute}</strong></p>
+       </article>
+      })
+     }
+    </section>
+
+
+
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App
---
+++ src/Heading.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div className='container'>
+     <section>
+        <h1>Quotes To Inspire</h1>
+      <p>(Input the number of qoutes required - 100 qoutes available)</p>
+
+
+     </section>
+    
+    </div>
+  )
+}
+
+export default Heading
